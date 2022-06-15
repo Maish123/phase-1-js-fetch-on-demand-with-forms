eb5f086618669a051658f6bde612457715edd18b
Denis Maina <Denis.maina@student.moringaschool.com>
2022-06-15 18:02:34 +0300

    updated file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-const init = () => {
-  
+const init = function(){
+   
+  const inputForm = document.querrySelector('form');
+
+inputForm.addEventListener('submit',function(event){
+    event.preventDefault();
+    const input = document.querySelector('input#searchByID');
+
+    fetch (`http://localhost:3000/movies/${input.value}`)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+        console.log(data)
+    })
+
+})
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
